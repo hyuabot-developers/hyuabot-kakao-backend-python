9bb4b782a9680813496b8bf7c6946b8446b1a23f
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-09-27 16:16:00 +0900

    Add func to know someone is new user

--- common/user.py
+++ common/user.py
@@ -1,7 +1,7 @@
 from firebase_admin import _apps, initialize_app, firestore
 
 from firebase.firebase_init import get_cred
-
+from kakao.common.sender import *
 
 # 봇 사용시 기존 사용자 정보 조회
 def get_user(user_id):
@@ -32,3 +32,21 @@ def update_user(user_key, campus):
     db = firestore.client()
     user = db.collection('botuser').document(user_key)
     user.update({'campus': campus})
+
+
+# 새 사용자 판별
+def find_is_new_user(user_id, answer):
+    block_id = '5eaa9bf741559f000197775d'
+    if '서울' in answer:
+        create_user(user_id, 1)
+        response = insert_text('서울캠퍼스로 설정되었습니다.')
+    elif 'ERICA' in answer:
+        create_user(user_id, 0)
+        response = insert_text('ERICA 캠퍼스로 설정되었습니다.')
+    else:
+        response = insert_text('등록되지 않은 사용자입니다.\n캠퍼스를 등록해 주십시오.')
+        reply = make_reply('서울', '서울캠퍼스 선택', block_id)
+        response = insert_replies(response, reply)
+        reply = make_reply('ERICA', 'ERICA 캠퍼스 선택', block_id)
+        response = insert_replies(response, reply)
+    return response
--- url.py
+++ url.py
@@ -7,7 +7,8 @@ from kakao.common.user import *
 from kakao.common.receiver import get_user_data  # To parse answer json
 from kakao.answer_shuttle import make_answer_shuttle_depart_info  # To get departure info
 from kakao.answer_shuttle import make_answer_shuttle_stop_detail  # To get shuttle stop info
-from kakao.answer_library import make_answer_reading_room_info # To get seat info in reading room
+from kakao.answer_library import make_answer_reading_room_info  # To get seat info in reading room
+from kakao.answer_food import make_answer_food_menu  # To get food menu
 from kakao.common.sender import *
 from kakao.common.models import kakao_url
 
@@ -28,7 +29,24 @@ class GetFood(Resource):
     # @kakao_url.expect(kakao_data)
     def post(self):
         """원하는 캠퍼스 또는 식당을 입력받아 식당별 메뉴를 반환합니다."""
-        return 'food'
+        user_id, answer = get_user_data(request.get_json())
+        user_info = get_user(user_id)
+        if not user_info:
+            response = find_is_new_user(user_id, answer)
+            return jsonify(response)
+        if '의 식단입니다.' in answer:
+            result_json = make_answer_food_menu(user_info['campus'], answer.split('의 식단입니다.')[0].strip())
+        else:
+            response = insert_text('원하는 식당을 선택해주세요.')
+            if user_info['campus']:
+                rest_list = ['학생식당', '신학생식당', '교직원식당', '신교직원식당', '제1생활관식당', '제2생활관식당', '사랑방', '행원파크']
+            else:
+                rest_list = ['학생식당', '교직원식당', '창의인재원식당', '푸드코트', '창업보육센터']
+            for restaurant in rest_list:
+                reply = make_reply(restaurant, '5eaa9b11cdbc3a00015a23fb', f'{restaurant}의 식단입니다.')
+                response = insert_replies(response, reply)
+            return jsonify(response)
+        return jsonify(result_json)
 
 
 @kakao_url.route('/library/seats')
@@ -38,11 +56,14 @@ class SearchBook(Resource):
         """원하는 캠퍼스 또는 열람실을 입력받아 열람실별 잔여 좌석을 반환합니다."""
         user_id, answer = get_user_data(request.get_json())
         user_info = get_user(user_id)
+        if not user_info:
+            response = find_is_new_user(user_id, answer)
+            return jsonify(response)
         if '열람실 정보' in answer:
-            seat_info = make_answer_reading_room_info(user_info['campus'])
+            response = make_answer_reading_room_info(user_info['campus'])
         else:
-            seat_info = make_answer_reading_room_info(user_info['campus'], answer[1:].split('의 좌석정보입니다.')[0].strip())
-        return jsonify(seat_info)
+            response = make_answer_reading_room_info(user_info['campus'], answer[1:].split('의 좌석정보입니다.')[0].strip())
+        return jsonify(response)
 
 
 @kakao_url.route('/update/campus')
@@ -61,18 +82,7 @@ class UpdateCampus(Resource):
                 update_user(user_id, 1)
                 response = insert_text('서울 캠퍼스로 변경되었습니다.')
         else:
-            if '서울' in answer:
-                create_user(user_id, 1)
-                response = insert_text('서울캠퍼스로 설정되었습니다.')
-            elif 'ERICA' in answer:
-                create_user(user_id, 0)
-                response = insert_text('ERICA 캠퍼스로 설정되었습니다.')
-            else:
-                response = insert_text('등록되지 않은 사용자입니다.\n캠퍼스를 등록해 주십시오.')
-                reply = make_reply('서울', '서울캠퍼스 선택', block_id)
-                response = insert_replies(response, reply)
-                reply = make_reply('ERICA', 'ERICA 캠퍼스 선택', block_id)
-                response = insert_replies(response, reply)
+            response = find_is_new_user(user_id, answer)
         return jsonify(response)
 
 
